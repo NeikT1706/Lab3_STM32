ccea445caca3e3833e138cf9d399d17fbaaeb651
NeikT1706 <vanhkhoa271176@gmail.com>
2023-11-23 20:45:14 +0700

    no message

--- STM32_Project/Core/Inc/main.h
+++ STM32_Project/Core/Inc/main.h
@@ -58,6 +58,46 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define EN0_Pin GPIO_PIN_0
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_1
+#define EN1_GPIO_Port GPIOA
+#define EN2_Pin GPIO_PIN_2
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_3
+#define EN3_GPIO_Port GPIOA
+#define Green1_Pin GPIO_PIN_4
+#define Green1_GPIO_Port GPIOA
+#define Yellow1_Pin GPIO_PIN_5
+#define Yellow1_GPIO_Port GPIOA
+#define Red1_Pin GPIO_PIN_6
+#define Red1_GPIO_Port GPIOA
+#define Green2_Pin GPIO_PIN_7
+#define Green2_GPIO_Port GPIOA
+#define SegA_Pin GPIO_PIN_0
+#define SegA_GPIO_Port GPIOB
+#define SegB_Pin GPIO_PIN_1
+#define SegB_GPIO_Port GPIOB
+#define SegC_Pin GPIO_PIN_2
+#define SegC_GPIO_Port GPIOB
+#define Yellow2_Pin GPIO_PIN_8
+#define Yellow2_GPIO_Port GPIOA
+#define Red2_Pin GPIO_PIN_9
+#define Red2_GPIO_Port GPIOA
+#define BTN0_Pin GPIO_PIN_10
+#define BTN0_GPIO_Port GPIOA
+#define BTN1_Pin GPIO_PIN_11
+#define BTN1_GPIO_Port GPIOA
+#define BTN2_Pin GPIO_PIN_12
+#define BTN2_GPIO_Port GPIOA
+#define SegD_Pin GPIO_PIN_3
+#define SegD_GPIO_Port GPIOB
+#define SegE_Pin GPIO_PIN_4
+#define SegE_GPIO_Port GPIOB
+#define SegF_Pin GPIO_PIN_5
+#define SegF_GPIO_Port GPIOB
+#define SegG_Pin GPIO_PIN_6
+#define SegG_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
---
+++ STM32_Project/Core/Src/global.c
@@ -0,0 +1,9 @@
+/*
+ * global.c
+ *
+ *  Created on: Oct 23, 2023
+ *      Author: Admin
+ */
+#include "global.h"
+int status = 1;
+
